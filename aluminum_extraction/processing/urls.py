ab9c134958b597efe975a784252fd0adebcec3c8
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2024-12-07 12:09:50 +0530

    Update urls.py

--- aluminum_extraction/processing/urls.py
+++ aluminum_extraction/processing/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views  # For the built-in logout view
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
@@ -8,5 +9,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('access_denied/', views.access_denied, name='access_denied'),
     path('api/get_processing_data/<int:processing_id>/', views.get_processing_data, name='get_processing_data'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # Add logout URL
 ]
  
